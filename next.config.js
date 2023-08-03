3119dd270d45077172c48ada2bd5f7a728b12c79
chojeongeun <juy0816@naver.com>
2023-08-03 10:11:02 +0900

    next초기세팅 (module.scss설정)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: false,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'style')],
+		prependData: `@import "styles/_variables.scss";`,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
